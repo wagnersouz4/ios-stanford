29fcf6ec3a5f20e6239610d72fd4dfa0a9fc19af
wagnersouz4 <wagnersouz4@gmail.com>
2017-03-05 12:35:04 -0300

    adding calculator's assets

---
+++ Calculator/Calculator/UICalculatorButton.swift
@@ -0,0 +1,21 @@
+//
+//  UICalculatorButton.swift
+//  Calculator
+//
+//  Created by Pan on 5/03/17.
+//  Copyright © 2017 Wagner Souza. All rights reserved.
+//
+
+import UIKit
+
+class UICalculatorButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
